f8e820e987bc255f35ecbc61b362a949aee1ebd0
bno1 <ealex95@gmail.com>
2017-12-03 01:01:55 +0200

    day02 solved

---
+++ src/day02.rs
@@ -0,0 +1,47 @@
+use std::io;
+use std::io::BufRead;
+use std::cmp;
+
+fn parse_line(l: String) -> Vec<i32> {
+    l.split_whitespace()
+     .map(|x| x.parse().unwrap())
+     .collect()
+}
+
+fn min_max(l: &Vec<i32>) -> i32 {
+    l.iter()
+     .fold(None,
+        |mm, x| mm.map(|(mi, ma)| (cmp::min(mi, x), cmp::max(ma, x)))
+                  .or(Some((x, x)))
+     )
+     .map_or(0, |(mi, ma)| ma - mi)
+}
+
+fn quotient(l: &Vec<i32>) -> i32 {
+    for i in 0..l.len() {
+        for j in (i + 1)..l.len() {
+            let a = cmp::max(l[i], l[j]);
+            let b = cmp::min(l[i], l[j]);
+
+            if a % b == 0 {
+                return a / b;
+            }
+        }
+    }
+
+    0
+}
+
+fn main() {
+    let stdin = io::stdin();
+
+    let matrix: Vec<Vec<i32>> = stdin.lock().lines()
+        .map(|l| l.map(parse_line).unwrap())
+        .collect();
+
+    let minmax_sum: i32 = matrix.iter().map(min_max).sum();
+    println!("Solution 1: {}", minmax_sum);
+
+    let quot_sum: i32 = matrix.iter().map(quotient).sum();
+    println!("Solution 2: {}", quot_sum);
+}
